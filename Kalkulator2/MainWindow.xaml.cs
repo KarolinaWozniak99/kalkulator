a77ccd586b725d3b2b17b5e8fcc4d982cb3ec75a
KarolinaWozniak99 <karolina.wozniak99@wp.pl>
2020-04-08 11:24:13 +0200

    Kalkulator-poprawa

--- Kalkulator2/MainWindow.xaml.cs
+++ Kalkulator2/MainWindow.xaml.cs
@@ -24,10 +24,59 @@ namespace Kalkulator2
         {
             InitializeComponent();
         }
+
         string liczba1 = "";
         string liczba2 = "";
         float wynik = 0;
+        string wynik2 = "";
+        bool czywynik = false;
         string operacja = "";
+
+       
+        private void Bloc()
+        {
+            BKropka.IsEnabled = false;
+            B0.IsEnabled = false;
+            B1.IsEnabled = false;
+            B2.IsEnabled = false;
+            B3.IsEnabled = false;
+            B4.IsEnabled = false;
+            B5.IsEnabled = false;
+            B6.IsEnabled = false;
+            B7.IsEnabled = false;
+            B8.IsEnabled = false;
+            B9.IsEnabled = false;
+        }
+
+        private void Unblock()
+        {
+            BKropka.IsEnabled = true;
+            B0.IsEnabled = true;
+            B1.IsEnabled = true;
+            B2.IsEnabled = true;
+            B3.IsEnabled = true;
+            B4.IsEnabled = true;
+            B5.IsEnabled = true;
+            B6.IsEnabled = true;
+            B7.IsEnabled = true;
+            B8.IsEnabled = true;
+            B9.IsEnabled = true;
+        }
+
+        private void BlockOp()
+        {
+            BOdejmowanie.IsEnabled = false;
+            BDodawanie.IsEnabled = false;
+            BMnozenie.IsEnabled = false;
+            BDzielenie.IsEnabled = false;
+        }
+        private void UnBlockOp()
+        {
+            BOdejmowanie.IsEnabled = true;
+            BDodawanie.IsEnabled = true;
+            BMnozenie.IsEnabled = true;
+            BDzielenie.IsEnabled = true;
+        }
 
         private void B7_Click(object sender, RoutedEventArgs e)
         {
@@ -40,12 +89,13 @@ namespace Kalkulator2
             {
                 liczba2 += "7";
                 WynikTexBox.Text = liczba2;
-
+                BlockOp();
             }
         }
 
         private void B8_Click(object sender, RoutedEventArgs e)
         {
+
             if (operacja == "")
             {
                 liczba1 += "8";
@@ -55,6 +105,7 @@ namespace Kalkulator2
             {
                 liczba2 += "8";
                 WynikTexBox.Text = liczba2;
+                BlockOp();
             }
         }
 
@@ -69,6 +120,7 @@ namespace Kalkulator2
             {
                 liczba2 += "9";
                 WynikTexBox.Text = liczba2;
+                BlockOp();
             }
         }
 
@@ -83,6 +135,7 @@ namespace Kalkulator2
             {
                 liczba2 += "4";
                 WynikTexBox.Text = liczba2;
+                BlockOp();
             }
         }
 
@@ -97,6 +150,7 @@ namespace Kalkulator2
             {
                 liczba2 += "5";
                 WynikTexBox.Text = liczba2;
+                BlockOp();
             }
         }
 
@@ -111,6 +165,7 @@ namespace Kalkulator2
             {
                 liczba2 += "6";
                 WynikTexBox.Text = liczba2;
+                BlockOp();
             }
         }
 
@@ -125,6 +180,7 @@ namespace Kalkulator2
             {
                 liczba2 += "1";
                 WynikTexBox.Text = liczba2;
+                BlockOp();
             }
         }
 
@@ -139,6 +195,7 @@ namespace Kalkulator2
             {
                 liczba2 += "2";
                 WynikTexBox.Text = liczba2;
+                BlockOp();
             }
         }
 
@@ -153,58 +210,94 @@ namespace Kalkulator2
             {
                 liczba2 += "3";
                 WynikTexBox.Text = liczba2;
+                BlockOp();
             }
         }
 
         private void B0_Click(object sender, RoutedEventArgs e)
         {
+            
             if (operacja == "")
             {
-                liczba1 += "0";
-                WynikTexBox.Text = liczba1;
+                if (liczba1 == "")
+                {
+                    liczba1 += "0";
+                    B0.IsEnabled = false;
+                    WynikTexBox.Text = liczba1;
+                    
+                }
+                else
+                {
+                    B0.IsEnabled = true;
+                    liczba1 += "0";
+                    WynikTexBox.Text = liczba1;
+                    
+                }
             }
             else
             {
-                liczba2 += "0";
-                WynikTexBox.Text = liczba2;
+                if (liczba2 == "")
+                {
+                    liczba2 += "0";
+                    B0.IsEnabled = false;
+                    WynikTexBox.Text = liczba2;
+                    BlockOp();
+                }
+                else
+                {
+                    B0.IsEnabled = true;
+                    liczba2 += "0";
+                    WynikTexBox.Text = liczba2;
+                    BlockOp();
+                }
             }
         }
 
         private void BKropka_Click(object sender, RoutedEventArgs e)
         {
-
-
-            
-            if (operacja == "")
+            if (liczba1 != "")
             {
-                liczba1 += ".";
-                WynikTexBox.Text = liczba1;
+
+                if (operacja == "")
+                {
+                    liczba1 += ".";
+                    BKropka.IsEnabled = false;
+                    WynikTexBox.Text = liczba1;                  
+                }
+                else
+                {
+                    if (liczba2 != "")
+                    {
+                        liczba2 += ".";
+                        BKropka.IsEnabled = false;
+                        WynikTexBox.Text = liczba2;
+                    }                  
+                }
             }
-            else
-            {
-                liczba2 += ".";
-                WynikTexBox.Text = liczba2;
-            }
-            
+            B0.IsEnabled = true;
         }
 
         private void BWynik_Click(object sender, RoutedEventArgs e)
         {
 
-            if (liczba1 != "" && liczba2 !="" )
+            if (liczba1 != "" && liczba2 != "")
             {
                 var l1 = Convert.ToDouble(liczba1);
                 var l2 = Convert.ToDouble(liczba2);
                 switch (operacja)
                 {
                     case "+":
-                        WynikTexBox.Text = (l1 + l2).ToString();
+                        wynik2 = (l1 + l2).ToString();
+                        WynikTexBox.Text = wynik2;
+                        wynik2 = liczba1;
                         break;
                     case "-":
                         WynikTexBox.Text = (l1 - l2).ToString();
+                        liczba1 = WynikTexBox.Text;
                         break;
                     case "*":
                         WynikTexBox.Text = (l1 * l2).ToString();
+                        liczba1 = WynikTexBox.Text;
                         break;
                     case "/":
                         if (l2 == 0)
@@ -213,20 +306,31 @@ namespace Kalkulator2
                         {
                             wynik = (float)(l1 / l2);
                             WynikTexBox.Text = wynik.ToString();
+                            liczba1 = WynikTexBox.Text;
                         }
                         break;
-            
+
                 }
             }
-            else
+            else if (operacja == "")
+            {
                 WynikTexBox.Text = liczba1;
+                BKropka.IsEnabled = false;
+                
+            }
+            liczba1 = WynikTexBox.Text;
+            wynik = 0;
+            liczba2 = "";
+            operacja = "";
+            Bloc();
+            UnBlockOp();
         }
 
         private void BDodawanie_Click(object sender, RoutedEventArgs e)
         {
             operacja = "+";
             WynikTexBox.Text = "";
-
+            Unblock();
         }
 
         private void BOdejmowanie_Click(object sender, RoutedEventArgs e)
@@ -248,7 +352,7 @@ namespace Kalkulator2
             {
                 operacja = "-";
                 WynikTexBox.Text = "";
-
+                Unblock();
             }
         }
 
@@ -256,19 +360,16 @@ namespace Kalkulator2
         {
             operacja = "*";
             WynikTexBox.Text = "";
+            Unblock();  
         }
 
         private void BDzielenie_Click(object sender, RoutedEventArgs e)
         {
             operacja = "/";
             WynikTexBox.Text = "";
+            Unblock();
         }
 
-        private void BProcent_Click(object sender, RoutedEventArgs e)
-        {
-            operacja = "%";
-            WynikTexBox.Text = "";
-        }
 
 
         private void BC_Click(object sender, RoutedEventArgs e)
@@ -278,6 +379,8 @@ namespace Kalkulator2
             wynik = 0;
             operacja = "";
             WynikTexBox.Text = "";
+            Unblock();
+
         }
 
         private void BAC_Click(object sender, RoutedEventArgs e)
@@ -286,11 +389,15 @@ namespace Kalkulator2
             {
                 liczba1 = "";
                 WynikTexBox.Text = "";
+                Unblock();
+
             }
             else
             {
                 liczba2 = "";
                 WynikTexBox.Text = "";
+                Unblock();
+
             }
 
         }
